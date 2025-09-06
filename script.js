8be3d8c292a6a64c81300e227de9f5359e9ab020
beeolly <belenkod55@gmail.com>
2025-09-06 15:45:33 +0300

    less  2/14

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+    rating = prompt('На сколько оцените его?', ''),
+    lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+    rating1 = prompt('На сколько оцените его?', '');
+      
+personalMovieDB.movies[lastFilm] = rating;
+personalMovieDB.movies[lastFilm1] = rating1;
+
+console.log(personalMovieDB);

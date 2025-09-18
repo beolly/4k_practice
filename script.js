59ad5c86e59027873caf7673303274604707e072
beeolly <belenkod55@gmail.com>
2025-09-18 13:09:27 +0300

    2 pract

--- script.js
+++ script.js
@@ -8,13 +8,52 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Один з останніх переглянутих фільмів?', ''),
+        rating = +prompt('На скільки оціните його?', '');
+        console.log(typeof(rating));
+    if (lastFilm != null && rating != null && lastFilm !='' && rating != '' &&  lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = rating;
+    } else {
+        i--;
+    }
+}
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    rating = prompt('На сколько оцените его?', ''),
-    lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-    rating1 = prompt('На сколько оцените его?', '');
-      
-personalMovieDB.movies[lastFilm] = rating;
-personalMovieDB.movies[lastFilm1] = rating1;
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто досить мало фільмів');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ви кіноман');
+}   else {
+    console.log('error');
+
+}
 
 console.log(personalMovieDB);
+
+// const a=25;
+// const arr=[23,63];
+
+// arr[2]=a;
+// (a>0) ? console.log('ok') : console.log('error');
+
+// switch (a) {
+//      case 10:
+//         console.log('error');
+//         break;
+
+//     case 26:
+//         console.log('ok');
+//         break;   
+//     default:
+//         console.log('default');
+//         break;      
+// }
+
+// const obj = {
+//     d:50 , 
+//     e:60
+// }
+// obj.l=54;
+// alert(obj.d);

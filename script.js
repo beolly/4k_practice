8e868c34b12bcbb2db629d257b65bf26b22d0085
beeolly <belenkod55@gmail.com>
2025-09-26 12:45:39 +0300

    fghj

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 "use strict";
 let numberOfFilms;
 
-function start() {
-  numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
-
-  while (
-    numberOfFilms === "" ||
-    numberOfFilms == null ||
-    isNaN(numberOfFilms)
-  ) {
-    numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
-  }
-}
-
-start();
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
-};
+  privat: false,
+  start:function () {
+ personalMovieDB.count = +prompt("Скільки фільмів ви вже переглянули?", "");
 
-function rememberMyFilms() {
+  while (
+    personalMovieDB.count === "" ||
+    personalMovieDB.count == null ||
+    isNaN(numberOfFilms)
+  ) {
+    personalMovieDB.count = +prompt("Скільки фільмів ви вже переглянули?", "");
+  }
+},
+
+rememberMyFilms: function() {
   for (let i = 0; i < 2; i++) {
     const lastFilm = prompt("Один з останніх переглянутих фільмів?", ""),
       rating = +prompt("На скільки оціните його?", "");
@@ -40,11 +36,8 @@ function rememberMyFilms() {
       i--;
     }
   }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
+},
+ detectPersonalLevel: function() {
   if (personalMovieDB.count < 10) {
     console.log("Переглянуто досить мало фільмів");
   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -54,9 +47,63 @@ function detectPersonalLevel() {
   } else {
     console.log("error");
   }
-}
+},
+ showMyDB: function(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }else {
+    console.log("privacy");
+  }
+},
 
-detectPersonalLevel();
+writeYourGenres:function() {
+  for (let i = 1; i <= 3; i++) {
+    let genre = prompt(`Ваш улюблений жанр під номером ${i}`);
+   
+    // альт варіант - через split
+    // i<2
+    // let genres = prompt(`Введіть ваші улюблені жанри через кому`);
+    // if (genres === "" || genres == null) { 
+    //   console.log("Ви ввели некоректні дані або не ввели їх взагалі");
+    //   i--;
+    // } else {
+    //   personalMovieDB.genres = genres.split(", ");
+    //  genres = genres.split(", ");
+    //  genres.sort();
+      //}
+  
+    
+    if (genre === "" || genre == null) {
+      console.log("Ви ввели некоректні дані або не ввели їх взагалі");
+      i--;
+    }else{
+      personalMovieDB.genres[i-1] = genre;
+    }
+  }
+  
+  personalMovieDB.genres.forEach((item, i) => {
+    console.log(`Улюблений жанр ${i + 1} - це ${item}`);
+  });
+
+
+},
+
+  toggleVisibleMyDB: function() {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  }
+
+};
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB();
+
+
+// console.log(personalMovieDB);
+
+
 
 // function showMyDB() {
 //   if (personalMovieDB.privat == false) {
@@ -66,23 +113,10 @@ detectPersonalLevel();
 //   }
 // }
 
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }else {
-    console.log("privacy");
-  }
-}
-showMyDB(personalMovieDB.privat);
 
 
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}`);
-  }
-}
-writeYourGenres();
-console.log(personalMovieDB);
+// Lessons
+
 // const a=25;
 // const arr=[23,63];
 
@@ -112,3 +146,56 @@ console.log(personalMovieDB);
 // let a='14';
 
 // console.log(parsefloat(a));
+
+
+// function fg(lang, callback) {
+//   console.log(`я вчу ${lang}`);
+//   callback();
+
+// }
+// function done() {
+//   console.log('я пройшов цей урок');
+// }
+// fg('JavaScript', done);
+// fg('Python', function() {
+//   console.log('я пройшов цей урок');
+// });
+// fg('Java', () => {
+//   console.log('я пройшов цей урок');
+// }); 
+
+// const obj={
+//     name:'fg',
+//     do:'gh',
+//     fg:55,
+//     gg:{
+//         fg:55,
+//         gg:55
+//     }
+// };
+// console.log(obj.fg);
+// for (let o in obj){
+//     console.log( `vhj ${o} in  ${obj[o]}` );
+// }
+
+//less
+// const arr=['hjk','jk','bj'];
+
+
+// arr.forEach(function(item,i,arr){
+//   console.log(`num ${i+1} fhj ${item }  in ${arr}`)
+// })
+// console.log(arr.join('/'));
+
+// less
+// const sold={
+//   name:'fg',
+//   do:'gh',
+//   fg:55,
+// };
+
+// const john= Object.create(sold);
+
+// john.name='John';
+// console.log(john);
+// console.log(john.fg);
